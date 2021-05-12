2f66b83718d706979219372b8d8867957a10f86f
Shadab.Umer <Shadab.Umer@LAP00215>
2021-05-12 15:34:44 +0530

    front-end changes updated

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button class="btn" :style="{background: color}"
+        @click="onClick()"
+    >{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String
+    },
+    methods: {
+        onClick() {
+            this.$emit('btn-click');
+        }
+    },
+}
+</script>
